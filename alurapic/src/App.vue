cd7dc57f5efc02f029358544087c05831187e515
Arlisson Menacho <Arlissonmenacho@gmail.com>
2021-12-02 23:40:56 -0400

    Iniciando git flow
    Seguindo alguns testes durante a aula modificando app.vue para exibição de imagens

---
+++ alurapic/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <h1 v-Text> titulo </h1>
+    <ul>
+        <li v-for="foto of fotos">
+            <p>{{foto.titulo}}</p>
+            <img :src="foto.url" :alt="foto.titulo">
+        </li>
+    </ul>
+</div>
+
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            titulo:'Text',
+            fotos: [
+            {
+                url:'https://cdn.dicionariopopular.com/imagens/meme-da-mulher-gritando-com-o-gato-na-mesa-og.jpg',
+                titulo: 'cachorro'
+            },
+            {
+                url:'https://1.bp.blogspot.com/-Gg8gS6MIsEQ/XYEV3cSKEEI/AAAAAAABOH8/LCRbWmmmGXUpCO3G9L2JmFC6P36Ugn5zACLcBGAsYHQ/s1600/1%25271%25271%25271.jpg',
+                titulo: 'Rau'
+            }
+        ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
