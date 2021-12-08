098913a337e8017437d1dc2db6fb82f52214e3d5
Arlisson Menacho <Arlissonmenacho@gmail.com>
2021-12-07 23:16:41 -0400

    Limitado escopo da estilização
    Limitando estilização componente.

--- alurapic/src/components/shared/painel/painel.vue
+++ alurapic/src/components/shared/painel/painel.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="painel">
-    <h2 class="painel-titulo">{{titulo}}</h2>
+    <h2 class="painel-titulo">{{ titulo }}</h2>
     <slot class="painel-conteudo"></slot>
   </div>
 </template>
 
 <script>
 export default {
-    props: ['titulo']
+  props: ["titulo"],
 };
 </script>
 
-<style>
-
+<style scoped>
 .painel {
   padding: 0 auto;
   border: solid 2px grey;
@@ -32,5 +31,9 @@ export default {
   margin: 0 0 15px 0;
   padding: 10px;
   text-transform: uppercase;
+}
+
+* {
+    box-shadow: 5px 5px 5px;
 }
 </style>
